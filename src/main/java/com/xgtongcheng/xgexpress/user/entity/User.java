23912e6f257ac9d17f844936bf3694b15d246fcd
waising <waising0204@gmail.com>
2015-04-28 16:10:39 +0800

    mybatis set

--- src/main/java/com/xgtongcheng/xgexpress/user/entity/User.java
+++ src/main/java/com/xgtongcheng/xgexpress/user/entity/User.java
@@ -1,9 +1,12 @@
 package com.xgtongcheng.xgexpress.user.entity;
 
+import java.io.Serializable;
+
 /**
  * Created by WWX on 2015/04/24.
  */
-public class User {
+public class User implements Serializable{
+    private static final long serialVersionUID = -4415990281535582814L;
     private String name;
     private String sex;
 
