cffd1592d5483cbdb1fd634dac549aeec7d85ef3
wwx <waising0204@gmail.com>
2015-08-31 08:40:41 +0800

    --

--- src/main/java/com/xgtongcheng/xgexpress/user/web/controller/UserController.java
+++ src/main/java/com/xgtongcheng/xgexpress/user/web/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.xgtongcheng.xgexpress.user.web.controller;
 
-import com.alibaba.druid.filter.AutoLoad;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.xgtongcheng.xgexpress.user.entity.User;
 import com.xgtongcheng.xgexpress.user.service.UserService;
 import org.slf4j.Logger;
@@ -27,6 +28,16 @@ public class UserController {
         List<User> list = userService.getUser();
         System.out.println(list.size());
         logger.info("111111111111111111111111111111111111");
+        map.addAttribute("user",list.get(0));
+        return "index";
+    }
+
+    @RequestMapping(value = "/list")
+    public String users(ModelMap map,int pageNum,int pageSize){
+        PageHelper.startPage(pageNum, pageSize);
+        List<User> list = userService.getUser();
+        System.out.println(list.size());
+        logger.info("Total:"+((Page)list).getTotal());
         map.addAttribute("user",list.get(0));
         return "index";
     }
--- target/xgexpress/WEB-INF/views/index.jsp
+++ target/xgexpress/WEB-INF/views/index.jsp
@@ -1,6 +1,7 @@
 <html>
 <body>
-<h2>Hello World1111123!</h2>
+<h2>Hello World!</h2>
 <h1>${user.name}</h1>
+<h1>${user.sex}</h1>
 </body>
 </html>
